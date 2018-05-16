77b1485895214f3d1d030cdfdc038ec3e5a2896a
Lorenzo Peña <lorinkoz@gmail.com>
2018-05-16 10:51:13 -0400

    Added settings for static files

--- mtpu/settings.py
+++ mtpu/settings.py
@@ -87,6 +87,8 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 WSGI_APPLICATION = 'mtpu.wsgi.application'
 
 
@@ -139,4 +141,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
