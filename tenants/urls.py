7e86d309ef5d65f9bb9997ad077416307391be53
Lorenzo Peña <lorinkoz@gmail.com>
2018-05-17 19:09:45 -0400

    Added some tenant views

--- tenants/urls.py
+++ tenants/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from . import views
+
 
 urlpatterns = [
+    path('', views.HomePage.as_view(), name='home'),
+    path('login/', views.LogInPage.as_view(), name='login'),
+    path('logout/', views.LogOutPage.as_view(), name='logout'),
+    # re_path('signup/(outsider|primary|support)/', views.SignUpPage.as_view(), name='signup'),
 ]
--- tenants/views.py
+++ tenants/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic import View, TemplateView, FormView, ListView, DeleteView
 
-# Create your views here.
+from . import models
+from users import forms as user_forms
+
+
+class HomePage(TemplateView):
+    template_name = 'tenants/home.html'
+    
+
+class LogInPage(FormView):
+    form_class = user_forms.LoginForm
+    template_name = 'tenants/login.html'
+    
+    def get_success_url(self):
+        return self.request.GET.get('next', reverse('tenants'))
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        return super().form_valid(form)
+
+
+class LogOutPage(View):
+    
+    def post(self, request):
+        if request.user.is_authenticated:
+            logout(request)
+        next = request.POST.get('next', reverse('home'))
+        return redirect(next)
