0087b4f0fcace27ba673a036a1c2dcf2f9770a50
Lorenzo Peña <lorinkoz@gmail.com>
2018-05-17 19:47:45 -0400

    Added basic login/signup flow for tenants

--- tenants/views.py
+++ tenants/views.py
@@ -17,7 +17,7 @@ class LogInPage(FormView):
     template_name = 'tenants/login.html'
     
     def get_success_url(self):
-        return self.request.GET.get('next', reverse('tenants'))
+        return self.request.GET.get('next', reverse('home'))
 
     def form_valid(self, form):
         login(self.request, form.get_user())
@@ -31,3 +31,40 @@ class LogOutPage(View):
             logout(request)
         next = request.POST.get('next', reverse('home'))
         return redirect(next)
+
+
+class SignUpPage(FormView):
+    form_class = user_forms.SignUpForm
+    success_url = reverse_lazy('login')
+    template_name = 'tenants/signup.html'
+    
+    def form_valid(self, form):
+        from random import Random
+        r = Random()
+        mode = self.kwargs['mode']
+        if mode == 'primary':
+            user = models.PrimaryStaffUser(
+                login=form.cleaned_data['login'],
+                display_name=form.cleaned_data['display_name'],
+                rank=r.choice(range(1, 10)),
+            )
+            user.save()
+            user.team.set(models.SupportStaffUser.objects.order_by('?')[:3])
+        elif mode == 'support':
+            user = models.SupportStaffUser(
+                login=form.cleaned_data['login'],
+                display_name=form.cleaned_data['display_name'],
+                role=r.choice([1, 2, 3, 4]),
+            )
+        else:
+            user = models.RegisteredUser(
+                login=form.cleaned_data['login'],
+                display_name=form.cleaned_data['display_name'],
+            )
+        user.set_password(form.cleaned_data['password1'])
+        user.save()
+        return super().form_valid(form)
+    
+    def get_context_data(self, **kwargs):
+        kwargs.update({'mode': self.kwargs['mode']})
+        return super().get_context_data(**kwargs)
